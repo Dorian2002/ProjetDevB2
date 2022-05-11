bc3cc8072458b314eef8ac03e4ef7a413ec456d8
DFONSECA <dorian.fonseca@ynov.com>
2022-05-11 11:19:48 +0200

    Profile modification for users

--- App/Controllers/AccountController.cs
+++ App/Controllers/AccountController.cs
@@ -3,34 +3,26 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Identity;
 using App.Models;
 using App.ViewModels;
+using App.Areas.Identity.Data;
 
 namespace App.Controllers;
 public class AccountController : Controller
 {
+    
+    private readonly ApplicationDbContext _dbContext;
     private readonly UserManager<ApplicationUser> userManager;
     private readonly SignInManager<ApplicationUser> signInManager;
-    public AccountController(UserManager<ApplicationUser> userManager,SignInManager<ApplicationUser> signInManager)
+    public AccountController(UserManager<ApplicationUser> userManager,SignInManager<ApplicationUser> signInManager, ApplicationDbContext dbContext)
     {
+        this._dbContext = dbContext;
         this.userManager = userManager;
         this.signInManager = signInManager;
     }
 
     [HttpGet]
-    public IActionResult Login()
-    {
-        return View("../Account/Login");
-    }
-    [HttpGet]
     public IActionResult Register()
     {
         return View("../Account/Register");
-    }
-
-    [HttpPost]
-    public async Task<IActionResult> Logout()
-    {
-        await signInManager.SignOutAsync();
-        return RedirectToAction("Index","Home");
     }
 
     [HttpPost]
@@ -53,6 +45,12 @@ public class AccountController : Controller
         return View(model);
     }
 
+    [HttpGet]
+    public IActionResult Login()
+    {
+        return View("../Account/Login");
+    }
+
     [HttpPost]
     public async Task<IActionResult> Login(LoginRequest model)
     {
@@ -66,5 +64,50 @@ public class AccountController : Controller
             ModelState.AddModelError(string.Empty, "Invalid Login Attempt");
         }
         return View(model);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Logout()
+    {
+        await signInManager.SignOutAsync();
+        return RedirectToAction("Index","Home");
+    }
+
+    [HttpGet]
+    public IActionResult Profile(int Id)
+    {
+        var user = userManager.FindByIdAsync(Id.ToString());
+        ProfileRequest model = new ProfileRequest{UserName=user.Result.UserName, Email=user.Result.Email};
+        return View("../Account/Profile", model);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Profile(ProfileRequest model)
+    {
+        if (ModelState.IsValid){
+            var user = userManager.FindByEmailAsync(model.Email).Result;
+            if (model.NewPassword != "" && model.NewPassword != null){
+                var newPasswordHash = userManager.PasswordHasher.HashPassword(userManager.FindByEmailAsync(model.Email).Result,model.NewPassword);
+                user.Email = model.Email;
+                user.UserName = model.UserName;
+                user.PasswordHash = newPasswordHash;
+                _dbContext.Entry(user).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                var result = await userManager.UpdateAsync(user);
+                await signInManager.SignInAsync(user, isPersistent: false);
+            }else{
+                user.Email = model.Email;
+                user.UserName = model.UserName;
+                _dbContext.Entry(user).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                var result = await userManager.UpdateAsync(user);
+                await signInManager.SignInAsync(user, isPersistent: false);
+            }
+            return RedirectToAction("Index", "Home");
+        }else{
+            var errors = ModelState.Values.SelectMany(v => v.Errors);
+            foreach(var e in errors){
+                Console.WriteLine(e.ErrorMessage);
+            }
+            return View(model);
+        }
     }
 }
---
+++ App/ViewModels/ProfileViewModel.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace App.ViewModels {
+    public class ProfileRequest {
+        [Required]
+        public string? UserName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        [DataType(DataType.Password)]
+        public string? NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name ="Confirm Password")]
+        [Compare("NewPassword",ErrorMessage ="Password and confirmation password not match.")]
+        public string? ConfirmNewPassword { get; set; }
+    }
+}
